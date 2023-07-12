85746079719a8521fcd4ef1a6d3cf81d303e9883
eddie-reyes <eddie.kingsreyes@gmail.com>
2023-07-12 00:17:56 -0700

    fixed multiline text

--- src/utils/TrendGraphic.ts
+++ src/utils/TrendGraphic.ts
@@ -2,7 +2,7 @@ import { Circle } from 'two.js/src/shapes/circle';
 import { Group } from 'two.js/src/group';
 import { DailyInstance } from './types';
 
-import { MultilineText } from './multiline-text/index';
+import { MultilineText } from 'twojs-multiline-text-fixed';
 
 import { two, getNonOverlappingPos, SIZE_FACTOR } from './graphics';
 
